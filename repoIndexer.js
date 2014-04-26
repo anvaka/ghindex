7eb775774ac793bf9d604d9597a2e7f1b9a36209
anvaka <anvaka@gmail.com>
2014-04-25 18:21:16 -0700

    This is the API I want

--- repoIndexer.js
+++ repoIndexer.js
@@ -1,45 +1,12 @@
-var Q = require('q');
 var tokens = require('./lib/tokens')();
-var JSONStream = require('JSONStream');
-var outStream = JSONStream.stringify();
-outStream.pipe(process.stdout);
 
 if (tokens.enabled > 0) {
-  var githubClient = require('./lib/ghclient')(tokens);
-  findRepositories(100000, []).fail(function(err) {
-    console.error(err);
-  });
+  require('./lib/ghclient')(tokens).findRepositories('stars:>=60000')
+    .on('data', function (repository) {
+      console.log(repository);
+    });
 } else {
   printTokenHelp();
-}
-
-var seen = {};
-function findRepositories(minStars) {
-  // github client can only process 1000 records. Split that into pages:
-  if (minStars) {
-    return githubClient.findRepo('stars:<' + (minStars + 1))
-       .then(pause(5000))
-       .then(processNextPage);
-  } else {
-    throw new Error('Min stars is required');
-  }
-
-  function processNextPage(repositories) {
-    var minWatchers;
-    for (var i = 0; i < repositories.length; ++i) {
-      var repo = repositories[i];
-      minWatchers = repo.watchers;
-      if (minWatchers >= 200) {
-        if (!seen[repo.name]) {
-          outStream.write(repo);
-          seen[repo.name] = true;
-        }
-      }
-    }
-
-    if (minWatchers && minWatchers >= 200) return findRepositories(minWatchers);
-    outStream.end();
-  }
 }
 
 function printTokenHelp() {
@@ -48,15 +15,4 @@ function printTokenHelp() {
     'Go to https://github.com/settings/applications and click "Create new token"',
     'Pass tokens as a comma-separated argument --tokens="A,B,C"'
   ].forEach(function (line) { console.log(line); });
-}
-
-function pause(timeMS) {
-  return function (result) {
-    var deferred = Q.defer();
-    setTimeout(function () {
-      deferred.resolve(result);
-    }, timeMS);
-
-    return deferred.promise;
-  };
 }
