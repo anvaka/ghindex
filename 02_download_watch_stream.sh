2438100d774a1f5264057a825d10bcdf205ada76
anvaka <anvaka@gmail.com>
2014-11-30 21:58:37 -0800

    Fixed wrong name

--- 02_download_watch_stream.sh
+++ 02_download_watch_stream.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ./scripts_config
 
-echo "Downloading watchers information from $DESTINATION_BUCKET"
+echo "Downloading watchers information from $WATCHERS_BUCKET"
 gsutil cp $WATCHERS_BUCKET ./
 
 echo "Extracting watchers information"
