ff556ab03664e038b593fbcdf3f832ec678f972a
anvaka <anvaka@gmail.com>
2014-11-30 20:49:03 -0800

    Extracted plain file name to config

--- 02_download_watch_stream.sh
+++ 02_download_watch_stream.sh
@@ -7,8 +7,5 @@ gsutil cp $DESTINATION_BUCKET ./
 echo "Extracting watchers information"
 gunzip $WATCHERS_FILE
 
-# After gunzip we should remove '.gz' extension:
-PLAIN_FILE=${WATCHERS_FILE/%.gz/}
-
-echo "Removing 'https://github.com/' prefix from $PLAIN_FILE"
-sed -i '' 's|https://github.com/||' $PLAIN_FILE
+echo "Removing 'https://github.com/' prefix from $PLAIN_WATCHERS_FILE"
+sed -i '' 's|https://github.com/||' $PLAIN_WATCHERS_FILE
